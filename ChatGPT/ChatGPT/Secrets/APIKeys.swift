00abda7e7b46477901dfdc7f4aae3fcabdb495fc
Gabriel Mors <gabrielmors210@gmail.com>
2024-10-10 14:21:04 -0400

    adicionado arquivo para apiKey

---
+++ ChatGPT/ChatGPT/Secrets/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  ChatGPT
+//
+//  Created by Gabriel Mors  on 10/10/24.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let authToken = "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"
+}
